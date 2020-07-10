4ee62dbcfb2a32b74545509516f9ba51c790bc74
Raihat Zaman Neloy <raihatneloy1992@gmail.com>
2020-07-10 11:34:02 +0000

    Fix 01 knapsack with prices

--- tips-series/iterative_dp/01-knapsack.cpp
+++ tips-series/iterative_dp/01-knapsack.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int weights[101];   // weights of different object
+int prices[101];    // prices of different object
 int n;              // n objects
 int dp[101][1001];  // memoization array
 
@@ -14,30 +15,11 @@ int solve(int pos, int weight) {
   int ans = solve(pos+1, weight);
 
   if (weights[pos] <= weight)
-    ans = max(ans, 1 + solve(pos+1, weight - weights[pos]));
+    ans = max(ans, prices[pos] + solve(pos+1, weight - weights[pos]));
 
   dp[pos][weight] = ans;
 
   return ans;
-}
-
-int solveIterative(int weight) {
-  for (int i=n;i>=0;i--) {
-    for (int j=0;j<=weight;j++) {
-      if (i == n)   dp[i][j] = 0;
-      if (j == 0)   dp[i][j] = 0;
-
-      int ans = dp[i+1][j];
-
-      if (weights[i] <= weight)
-        ans = max(ans, 1 + dp[i+1][j - weights[i]]);
-
-      dp[i][j] = ans;
-    }
-  }
-
-  // solve(0, weight);
-  return dp[0][weight];
 }
 
 int main() {
